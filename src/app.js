600a641d92a3ec0ac8f21f70fe3124b4409e6f46
IkerMoBu <92671501+Ikerichu@users.noreply.github.com>
2025-11-07 18:20:03 +0000

    Fin

--- src/app.js
+++ src/app.js
@@ -7,5 +7,14 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  for(let i=0; pronoun.length > i; i++){
+    for(let x=0; adj.length > x; x++){
+      for(let z=0; noun.length > z; z++){
+        console.log(pronoun[i]+adj[x]+noun[z]+".com");
+      }
+    }
+  }
 };
